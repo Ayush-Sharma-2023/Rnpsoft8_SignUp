0a4c7cb26c4f2ca1f53c8698338c788fe9c6492b
Rajveer Shaw <rajveershaw2003@gmail.com>
2024-06-10 12:56:09 +0530

    Added select the language page

--- src/components/Language_selection.jsx
+++ src/components/Language_selection.jsx
@@ -1,9 +1,33 @@
+import "./Language_selection.css"
 import style from "./Language_selection.module.css"
+
 
 function LanguageSelect() {
     return (
         <>
             <div className={style.container}>
+                <center><p className={style.heading}>Select the language</p>
+                    <form action="index.html">
+                        <div className={style.all_lang}>
+                            <div className={style.all_lang_containers}>
+                                <input type="radio" id="Odia" name="language" value="Odia" />
+                                <label for="Odia">Odia</label>
+                            </div>
+                            <div className={style.all_lang_containers}>
+                                <input type="radio" id="English" name="language" value="English" />
+                                <label for="English">English</label>
+                            </div>
+                            <div className={style.all_lang_containers}>
+                                <input type="radio" id="Hindi" name="language" value="Hindi" />
+                                <label for="Hindi">Hindi</label>
+                            </div>
+                        </div>
+                        <textarea id="txtArea" rows="5" cols="55" placeholder="Enter the text"></textarea>
+                        <input type="file" id="custom-file-upload" />
+                        <label for="custom-file-upload" className="chooseFiles">Choose Files</label>
+                        <input type="submit" value="NEXT"/>
+                    </form>
+                </center>
                 <div className={style.eclipse}></div>
             </div>
         </>
